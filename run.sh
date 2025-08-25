b96d305db87382007b4b383250b9d540a3913f58
cme <info@cme-robotics.com>
2025-08-25 11:49:23 +0200

    Adding docker socket.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # cmeresearch_amr_webcontrol Dockerfile
 
-docker run -it --rm --name cmexa-webcontrol --network="host" --privileged --pid=host cmexa/webcontrol:1.0
+docker run -it --rm --name cmexa-webcontrol --network="host" -v /var/run/docker.sock:/var/run/docker.sock cmexa/webcontrol:1.0
